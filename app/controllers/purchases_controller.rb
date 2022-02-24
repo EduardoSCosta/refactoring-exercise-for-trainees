74e6d6337d48ff0f99ef3d70dc5092900b4732ec
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-02-24 18:25:47 -0300

    create method to return error messages

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,19 +2,19 @@ class PurchasesController < ApplicationController
   def create
     (valid_gateway, gateway_error) = ValidateGatewayService.call(purchase_params[:gateway])
 
-    return render json: { errors: gateway_error }, status: :unprocessable_entity unless valid_gateway
+    return error_message(gateway_error) unless valid_gateway
 
     (cart, cart_error) = FindCartService.call(purchase_params[:cart_id])
 
-    return render json: { errors: cart_error }, status: :unprocessable_entity unless cart
+    return error_message(cart_error) unless cart
 
     (user, user_errors) = CreateUserService.call(purchase_params[:user], cart)
 
-    return render json: { errors: user_errors }, status: :unprocessable_entity unless user_errors.nil?
+    return error_message(user_errors) unless user_errors.nil?
 
     (order, order_errors) = CreateOrderService.call(user, address_params, cart)
 
-    return render json: { errors: order_errors }, status: :unprocessable_entity unless order_errors.nil?
+    return error_message(order_errors) unless order_errors.nil?
 
     render json: { status: :success, order: { id: order.id } }, status: :ok
   end
@@ -32,5 +32,9 @@ class PurchasesController < ApplicationController
 
   def address_params
     purchase_params[:address] || {}
+  end
+
+  def error_message(error)
+    render json: { errors: error }, status: :unprocessable_entity
   end
 end
