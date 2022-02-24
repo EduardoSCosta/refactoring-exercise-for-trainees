71cf3b23646f135aaac1b9f32e25a48618143a80
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-02-24 09:42:54 -0300

    update ValidateGatewayService spec to use subject

--- spec/services/validate_gateway_service_spec.rb
+++ spec/services/validate_gateway_service_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ValidateGatewayService do
   describe '#call' do
+    subject(:call) { ValidateGatewayService.call(gateway) }
     context 'when gateway is valid' do
       let(:gateway) { 'paypal' }
       it 'returns true' do
-        expect(ValidateGatewayService.call(gateway)).to eq(true)
+        expect(call).to eq(true)
       end
     end
 
     context 'when gateway is invalid' do
       let(:gateway) { 'not_a_gateway' }
       it 'returns false' do
-        expect(ValidateGatewayService.call(gateway)).to eq(false)
+        expect(call).to eq(false)
       end
     end
   end
